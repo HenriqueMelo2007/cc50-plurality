15e35fd1417783adc14b835d2f39c820228f8ca1
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-03-04 15:08:03 -0300

    feat: add loop

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
   char candidateName[100];
   int votes;
 } Candidate;
 
-int voters;
+
 
 int main(int argc, char const *argv[])
 {
@@ -15,6 +16,22 @@ int main(int argc, char const *argv[])
     printf("Define at least one candidate.");
     return 1;
   }
+  
+  int voters;
+  int numberOfCandidates = argc - 1;
+
+  printf("Number of voters:");
+  scanf("%i", &voters);
+
+  Candidate candidates[voters];
+
+  for (int i = 0; i < numberOfCandidates; i++) {
+    strcpy(candidates[i].candidateName, argv[i + 1]);
+    candidates[i].votes = 0;
+  }
+
+
+  
   
   return 0;
 }
