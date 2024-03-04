afa533e898b852641344d4a664b7f9fd8f20c672
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-03-04 14:37:08 -0300

    feat: add a variable to count the voters and a struct fot the candidates

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+  char candidateName[100];
+  int votes;
+} Candidate;
+
+int voters;
+
 int main(int argc, char const *argv[])
 {
+
+  if (argc < 2) {
+    printf("Define at least one candidate.");
+    return 1;
+  }
   
   return 0;
 }
